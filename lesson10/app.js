aed06954287ff82f046da46273e8d219be89d251
aforawesome <35620345+aforawesome@users.noreply.github.com>
2018-03-17 14:23:15 +0800

    Update app.js
    j

--- lesson10/app.js
+++ lesson10/app.js
@@ -246,7 +246,7 @@ function endGame(){
 
 //define collectDiamond
 function collectDiamond(player,diamond){
-  score += 1000000000;
+  score += 1000000;
   scoretext.setText(score);
   diamond.kill();
 
